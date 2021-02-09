6c4ef015f6ae3b9c0432899b4f124bb56b0cabe4
bkevin1052 <31713984+bkevin1052@users.noreply.github.com>
2021-02-09 17:00:44 -0600

    Agregando pantallas

--- ZuperApp/src/app/Components/forgotpassword/forgotpassword.component.ts
+++ ZuperApp/src/app/Components/forgotpassword/forgotpassword.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forgotpassword',
@@ -7,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotpasswordComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+
+    private router: Router
+
+  ) {  }
+
+
+  resetPassword(){
+    this.router.navigate(['login']);
+  }
+
 
   ngOnInit() {
   }
