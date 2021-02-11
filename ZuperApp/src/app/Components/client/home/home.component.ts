41b56206ce7a9d6c3734d38d23468f8339a9790e
bkevin1052 <31713984+bkevin1052@users.noreply.github.com>
2021-02-11 11:10:01 -0600

    Agregando cambios

--- ZuperApp/src/app/Components/client/home/home.component.ts
+++ ZuperApp/src/app/Components/client/home/home.component.ts
@@ -1,3 +1,4 @@
+import { analyzeAndValidateNgModules } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -15,22 +16,6 @@ export class HomeComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+
   }
-
-
-  getFoto(servicio: any){
-
-    if (servicio.nombre === 'Plomeria') {
-      return 'https://raw.githubusercontent.com/bkevin1052/Handy/master/Splash%20screen%20icons/leaking.png'
-    } else if (servicio.nombre === 'Pintura') {
-      return 'https://raw.githubusercontent.com/bkevin1052/Handy/master/Splash%20screen%20icons/paint-roller.png'
-    } else if (servicio.nombre === 'Reparacion'){
-      return 'https://raw.githubusercontent.com/bkevin1052/Handy/master/Splash%20screen%20icons/wrench.png'
-    } else if (servicio.nombre === 'Limpieza') {
-      return 'https://raw.githubusercontent.com/bkevin1052/Handy/master/Home%20icon/cleanin.png'
-    } else {
-      return
-    }
-  }
-
 }
